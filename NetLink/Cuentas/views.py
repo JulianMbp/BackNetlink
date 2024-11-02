96f731ed66673728d50732dc94988a514ef60f4b
JulianMbp <julian_mtb993@outlook.es>
2024-11-02 17:02:11 -0500

    env

--- NetLink/Cuentas/views.py
+++ NetLink/Cuentas/views.py
@@ -284,7 +284,6 @@ class UsuariosView(APIView):
             return Response({'message': 'Usuario eliminado correctamente'}, status=status.HTTP_204_NO_CONTENT)
         except Usuario.DoesNotExist:
             return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
-          
 class UsuarioQueryApiView(APIView):
     def get(self, request, pkid, *args, **kargs):
         miUsuario=Usuario.objects.filter(id=pkid).first()
