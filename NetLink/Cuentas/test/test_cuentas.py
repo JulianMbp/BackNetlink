d9e0413875ad869b01fa0a8272013de850b0d81e
Hamilton Insandara <hamisan36@gmail.com>
2024-11-23 21:40:02 -0500

    Hi

---
+++ NetLink/Cuentas/test/test_cuentas.py
@@ -0,0 +1,104 @@
+import json
+from django.test import TestCase
+from django.urls import reverse
+from Cuentas.models import *
+
+class test_cuentasl(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        experience = Experience.objects.create(
+            company="a company",
+            position="a position",
+            description="a description"
+        )
+        labinfodata = [experience]
+        labInfo=laboralInformation.objects.create(
+            latestPosition=experience,
+            abilities=['An Ability'],
+            lookingForEmployement=True,
+            desiredPosition="A good position",
+            desiredCountry="A country",
+            telecommuting=True
+        )
+        labInfo.previousExperiences.set(labinfodata)
+        
+    def tearDown(self):
+        pass
+    
+    def test_view_laboral_list(self):
+        response=self.client.get('/api/Netlink/Llist')
+        data=json.loads(response.content.decode('utf-8'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(data), 0)
+    
+    def test_create_laboral(self):
+        response = self.client.post(
+            '/api/Netlink/laboralInfoAdd',
+            data= json.dumps({
+                "latestPosition":{
+                        "company": "A Company",
+                        "position": "A position",
+                        "description": "It's a very important position"
+                    },
+                "abilities":["Ability"],
+                "previousExperiences":[{
+                        'company': 'A Company',
+                        "position": "A position",
+                        "description": "It's a very important position"
+                    }],
+                "lookingForEmployement":True,
+                "desiredPosition":"A great position",
+                "desiredCountry":"un country",
+                "telecommuting":True
+            }),
+            content_type='application/json'
+        )
+        self.assertIn(response.status_code, (200, 201))
+        filtered_laboral=laboralInformation.objects.filter(desiredPosition='A great position').first()
+        self.assertEqual(filtered_laboral.desiredCountry, 'un country')
+
+class test_cuentasA(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        academicInfo=AcademicInformation.objects.create(
+            educativeInstitution='An institution',
+            title='a title',
+            academicDiscipline='an academic disc',
+            startDate="2024-11-01",
+            endDate="2024-11-01",
+            aditionalActivities=['An activity'],
+            description ='A description',
+            abilities=['An Ability'],
+        )
+        
+    def tearDown(self):
+        pass
+    
+    def test_view_academic_list(self):
+        response=self.client.get('/api/Netlink/academicList')
+        data=json.loads(response.content.decode('utf-8'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(data), 0)
+    
+    def test_create_academic(self):
+        response = self.client.post(
+            '/api/Netlink/academicInfoAdd',
+            data=json.dumps({
+                "educativeInstitution":"Another insti",
+                "title":"another title",
+                "academicDiscipline":"another disc",
+                "startDate":"2024-11-01",
+                "endDate":"2024-11-01",
+                "aditionalActivities":["Another one"],
+                "description" :"Another desc",
+                "abilities":["Another one"]
+            }),
+            content_type='application/json'
+        )
+        self.assertIn(response.status_code, (200, 201))
+        filtered_academic=AcademicInformation.objects.filter(educativeInstitution='Another insti').first()
+        self.assertEqual(filtered_academic.title, 'another title')
--- NetLink/api_publicacion/tests.py
+++ NetLink/api_publicacion/tests.py
@@ -1,3 +1,54 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
+from api_publicacion.models import Publicacion
 
-# Create your tests here.
+class test_publicacion(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Publicacion.objects.create(
+            titulo='Test Título', 
+            descripcion='Test Descripción',
+            multimedia='',
+            )
+    def tearDown(self):
+        pass
+class test_publicacion_list(test_publicacion):
+    def test_view_publicacion_list(self):
+        response=self.client.get(reverse('publicacion_list'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(len(response.data),1)
+    def test_create_publicacion(self):
+        response=self.client.post(reverse('publicacion_list'),{
+            'titulo':'Test Título',
+            'descripcion':'Test Descripción',
+            'multimedia':'',
+        })
+        self.assertEqual(response.status_code,[200,201])
+        filtered_publicacion=Publicacion.objects.filter(titulo='Test Título').first()
+        self.assertEqual(filtered_publicacion.descripcion,'Test Descripción')
+    def test_update_publicacion(self):
+        mi_publicacion=Publicacion.objects.create(
+            titulo='Test Título',
+            descripcion='Test Descripción',
+            multimedia='',
+        )
+        valid_publicacion={
+            'titulo':'Título',
+            'descripcion':'Descripción',
+            'multimedia':'',
+        }
+        url=reverse('actualizar_publicacion',kwargs={'pkid':mi_publicacion.id})
+        valid_publicacion_json=json.dumps(valid_publicacion)
+        response=self.client.put(url,valid_publicacion_json,content_type='application/json')
+        self.assertEqual(response.status_code,[200,201])
+    def test_delete_publicacion(self):
+        mi_publicacion=Publicacion.objects.create(
+            titulo='Test Título',
+            descripcion='Test Descripción',
+            multimedia='',
+        )
+        url=reverse('eliminar_publicacion',kwargs={'pkid':mi_publicacion.id})
+        response=self.client.delete(url)
+        self.assertEqual(response.status_code,204)
+        self.assertFalse(Publicacion.objects.filter(titulo='Test Título').exists())
